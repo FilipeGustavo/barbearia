b9fbef3a1e39e9c72a6a69df230397b2020f08a1
Filipe Gustavo <filipegustavofglv@gmail.com>
2018-09-19 21:57:12 -0300

    PrimeiroCommit

---
+++ Barbearia/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barbearia.Controllers
+{
+    public class LoginController : Controller
+    {
+        //// GET: Login
+        ///// <param name="returnURL"></param>
+        ///// <returns></returns>
+        //public ActionResult Login(string returnURL)
+        //{
+        //    /*Recebe a url que o usuário tentou acessar*/
+        //    ViewBag.ReturnUrl = returnURL;
+        //    return RedirectToAction("Index", "Agenda");
+        //}
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
